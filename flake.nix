5061c89799e9cef21e47bfdb89fae991be2a1d91
Nawamin M. <naminx@gmail.com>
2026-03-09 21:43:11 +0700

    feat: add direnv + devShell with podman for docker build workflow

--- flake.nix
+++ flake.nix
@@ -5,10 +5,27 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.default = import ./default.nix { 
+  outputs = { self, nixpkgs }:
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in
+  {
+    packages.x86_64-linux.default = import ./default.nix {
       system = "x86_64-linux";
       pkgsSrc = nixpkgs;
+    };
+
+    # Run `nix develop` to get a shell with podman for the Docker build workflow.
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = [
+        pkgs.podman
+        pkgs.bash
+        pkgs.coreutils
+      ];
+      shellHook = ''
+        echo "🐳 Podman $(podman --version) ready."
+        echo "   Run: ./build-docker.sh <CHROMIUM_VERSION>"
+      '';
     };
   };
 }
